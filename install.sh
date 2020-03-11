73d04774ca221b983314a31af9140d6bd006edb8
easaw <easaw@users.noreply.github.com>
2020-03-11 09:10:04 +0000

    Update to PHP 7.4

--- install.sh
+++ install.sh
@@ -18,13 +18,13 @@ LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 
 apt update
 
-apt install php7.3 php7.3-cli php7.3-common
+apt install php7.4 php7.4-cli php7.4-common
 
 #add most ommon PHP modules
 
-apt install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-mysql php7.3-xml php7.3-fpm libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl
+apt install php-pear php7.4-curl php7.4-dev php7.4-gd php7.4-mbstring php7.4-zip php7.4-mysql php7.4-xml php7.4-fpm libapache2-mod-php7.4 php7.4-imagick php7.4-recode php7.4-tidy php7.4-xmlrpc php7.4-intl
 
-a2enmod php7.3
+a2enmod php7.4
 
 ufw allow in "Apache Full"
 
